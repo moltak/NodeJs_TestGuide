a424acf6fbc6e7638d411b55e5d2656e204d9b58
moltak <moltak@gmail.com>
2016-09-29 23:53:46 +0900

    fail

--- test/acceptance_test.js
+++ test/acceptance_test.js
@@ -5,14 +5,14 @@ var should = require('should');
 
 var app = require('../index.js');
 
-describe('BDD style test', function () {
+describe('Acceptance test', function () {
 	it("should print 'Hello World'", function (done) {
 		request(app)
 			.get('/')
 			.set('Content-Type', 'application/json')
 			.expect(200)
 			.end(function (err, result) {
-				should.not.exist(err);
+				should.exist(err);
 				result.text.should.equal('Hello code states');
 				done();
 			});
